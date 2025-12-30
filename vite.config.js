91f3ace90ec527b1018fc0692718f592425c5871
Bing Hong Lin <linskybing@gmail.com>
2025-12-30 17:53:17 +0800

    style: prettier: format remaining files

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,21 @@ import svgr from 'vite-plugin-svgr';
 import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        svgr({
-            svgrOptions: {
-                icon: true,
-                // This will transform your SVG to a React component
-                exportType: 'named',
-                namedExport: 'ReactComponent',
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@nthucscc/ui': resolve(__dirname, 'packages/ui/src/index.ts'),
-            '@nthucscc/utils': resolve(__dirname, 'packages/utils/src/index.ts'),
-        },
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        // This will transform your SVG to a React component
+        exportType: 'named',
+        namedExport: 'ReactComponent',
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@nthucscc/ui': resolve(__dirname, 'packages/ui/src/index.ts'),
+      '@nthucscc/utils': resolve(__dirname, 'packages/utils/src/index.ts'),
     },
+  },
 });
